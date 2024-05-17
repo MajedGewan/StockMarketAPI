cf5054266abd3fd78248c462b286ea59cdb067e1
Majed Alhanash <majed.s.alhanash@gmail.com>
2024-05-17 14:43:30 +0400

    remove prints

--- yahoo_finance/connection_data_helper.py
+++ yahoo_finance/connection_data_helper.py
@@ -16,22 +16,15 @@ def get_raw_data(symbol, interval, period):
         for attempt in range(3):  # Retry up to 5 times
             response = requests.get(link, params=params, headers=headers)
             if response.status_code == 200:
-                print('4 - If works well')
                 raw_data = response.json()
                 if 'timestamp' not in raw_data['chart']['result'][0]:
-                    print('problem in timestamp')
-                    print(raw_data)
                     return raw_data, 404
                     
                 return raw_data, None 
             elif response.status_code == 429:
-                print('4 - If didnot works well 429')
-                print('Error: ', response.reason)
                 time.sleep(2 ** attempt)  # Exponential back-off
             else:
                 error = response.status_code
-                print('4 - If didnot works well')
-                print('Error: ', response.reason)
                 return None, error
         if raw_data is None:
             error = "429 unknow request code"
@@ -39,22 +32,14 @@ def get_raw_data(symbol, interval, period):
         return raw_data, error
 
 def get_data(symbol, period):
-     print('2 - Inside before fetching')
      chart_data, currency, regular_market_time, timezone, previous_close, high, low = None, None, None, None, None, None, None
      interval = get_interval(period)
-     print('3 - Inside before get data')
      data, error = get_raw_data(symbol, interval, period)
-     print('5 - After raw data')
      if error is None:
           chart_data, currency, regular_market_time, timezone, previous_close, high, low = process_data(data, interval)
-          print('6 - After process data')
-     if error is not None:
-         print(error)
-         print('therer was an error')
      return error, chart_data, currency, regular_market_time, timezone, previous_close, high, low
 def process_data(data,interval):
      result = data['chart']['result'][0]
-     print(result)
      meta = result['meta']
      indicators = result['indicators']
      timestamp = result['timestamp']
@@ -62,7 +47,6 @@ def process_data(data,interval):
      currency = meta['currency']
      regular_market_time = meta['regularMarketTime']
      timezone = meta['exchangeTimezoneName']
-     print('before data frame')
      high, low, previous_close = get_high_low_close(meta, numerical_data, interval)
      chart_data = pd.DataFrame({'Date':timestamp,
                    'High':numerical_data['high'],
@@ -71,7 +55,6 @@ def process_data(data,interval):
                    'Low':numerical_data['low'],
                    'Open':numerical_data['open']})
      chart_data['Date'] = chart_data['Date'].apply(pd.Timestamp, unit='s', tz=timezone)
-     print('after applying date')
      return chart_data, currency, regular_market_time, timezone, previous_close, high, low
     
      
